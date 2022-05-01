7faabe0c39922109afae89364071b18aaf47292d
Niccatania <Niccatania6@gmail.com>
2022-04-30 21:12:05 -0700

    add files

--- index.js
+++ index.js
@@ -1,40 +1,145 @@
 const inquirer= require("inquirer");
 const fs= require("fs");
-const employee = require('./lib/Employee');
-const manager = require('./lib/Manager');
-const Engineer = require('.lib/Engineer');
-const Intern= require('.lib/Intern');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+// const Engineer = require('.lib/Engineer');
+// const Intern= require('.lib/Intern');
+
+const team= []
 
 inquirer
 .prompt([
     {
-    // Assuming object class will be used
     type: "input",
-      name: "manager",
+      name: "manName",
       message: "Manager name:",
-},
-  {    type: "list",
-      name: "employeeChoice",
-      message: "Which employee type would you like to add?",
-      choices: [
+    },
+    {
+      type: "input",
+      name: "manID",
+      message: "ID:",},
+      {
+     
+      type: "input",
+      name: "manEmail",
+      message: "Email:",
+      },
+      {
+      type: "input",
+      name: "manNum",
+      message: "Manager office number:",
+
+      }])
+      .then(function(answers){
+        let manager=new Manager(answers.manName, answers.manID, answers.manEmail, answers.manNum)
+        team.push(manager)
+     console.log(manager);
+      nextUp()});
+       
+    
+function nextUp(){
+  inquirer
+.prompt([
+  {
+type:"list",
+name:"employeeChoice",
+message: "Which employee type would you like to add?",
+choices: [
           {
             name: "Engineer",
           },
           {
             name: "Intern",
+          },
+          {
+            name: "teamComplete!"
           }]
+        },
+        {
+          type:"input",
+          name:"gitHub",
+          message:"What is the Engineers github userName?:",
+          when:(answers) => answers.employeeChoice === "Engineer"
+        },
+        {
+          type:"input",
+          name:"School",
+          message:"What is the Interns School?:",
+          when:(answers) => answers.employeeChoice === "Intern"
+        },
+        {
+          type:"function",
+          name:generateHTML(),
+          when:(answers) => answers.employeeChoice === "teamComplete!"
         }
-// if else? 
+
+])
+  // Enginfo()
+
+}
+// if choice====Engineer 
+// inquirer
+// .prompt([
+//   {
+//   // Assuming object class will be used
+//   type: "input",
+//     name: "manName",
+//     message: "Manager name:",
+//   },
+//   {
+//     type: "input",
+//     name: "manID",
+//     message: "ID:",},
+//     {
+   
+//     type: "input",
+//     name: "manEmail",
+//     message: "Email:",
+//     },
+//     {
+//     type: "input",
+//     name: "manHub",
+//     message: "What is the Engineers github userName?:",
+
+//     }])
+// else choice === Intern
+// // inquirer
+// .prompt([
+//   {
+//   // Assuming object class will be used
+//   type: "input",
+//     name: "manName",
+//     message: "Intern name:",
+//   },
+//   {
+//     type: "input",
+//     name: "manID",
+//     message: "ID:",},
+//     {
+   
+//     type: "input",
+//     name: "manEmail",
+//     message: "Email:",
+//     },
+//     {
+//     type: "input",
+//     name: "school",
+//     message: "What is the Interns School:",
+
+// else if { generateHTML();
+
+// }
 
 
 
-    ])
-
-.then(answers=> {
-  console.info("Answers:", answers)
-})
+// .then(answers=> {
+//   console.info("Answers:", answers)
+// })
 
 
     // First attempt
-
-
+function generateHTML()
+{
+  console.log(manager)
+}
+// Write to FS
---
+++ tests/Employee.test.js
@@ -0,0 +1,12 @@
+const EmployeeTest = require(".lib/Employee");
+const Employee = require("../lib/Employee");
+
+describe("Employee",() => {
+    
+})
+
+
+it("should return an employee name", () => {
+   var nic = new Employee()
+  });
+  return this.name;
